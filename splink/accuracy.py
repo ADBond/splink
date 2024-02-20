af0d483f925e2c95ef70aa191e0922eabe301b35
ADBond <48208438+ADBond@users.noreply.github.com>
2024-02-20 15:56:53 +0000

    more splink 3 changes

--- splink/accuracy.py
+++ splink/accuracy.py
@@ -47,6 +47,7 @@ def truth_space_table_from_labels_with_predictions_sqls(
 
     sql = """
     select
+        some_splink_3_column,
         truth_threshold,
         count(*) as num_records_in_row,
         sum(c_P) as c_P,
