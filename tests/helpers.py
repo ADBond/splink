75a51a55e8a4d477d5796c06783c96257118030d
ADBond <48208438+ADBond@users.noreply.github.com>
2025-08-12 16:12:54 +0100

    test helper - not pandas for duckdb

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,6 +2,7 @@ import sqlite3
 from abc import ABC, abstractmethod
 from collections import UserDict
 
+import duckdb
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.types import INTEGER, TEXT
 
@@ -51,12 +52,23 @@ class TestHelper(ABC):
 
 
 class DuckDBTestHelper(TestHelper):
+    con = duckdb.connect()
+
     @property
     def DatabaseAPI(self):
         return DuckDBAPI
 
+    def db_api_args(self):
+        return {"connection": self.con}
+
     def convert_frame(self, df):
         return df
+
+    def load_frame_from_csv(self, path):
+        return self.con.read_csv(path)
+
+    def load_frame_from_parquet(self, path):
+        return self.con.read_parquet(path)
 
     @property
     def date_format(self):
