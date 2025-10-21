9d5834b1560fd2f76ae90ec8a5bfdf86f5b13df1
ADBond <48208438+ADBond@users.noreply.github.com>
2025-10-21 08:25:07 +0100

    remove dummy breaking test

--- tests/test_dummy_dependency_breaking.py
+++
@@ -1,7 +0,0 @@
-import sqlglot
-
-
-def test_sqlglot_version():
-    major, minor, patch = sqlglot.__version__.split(".")
-    absolute_version = 10_000 * int(major) + 100 * int(minor) + int(patch)
-    assert absolute_version <= 27_26_00
